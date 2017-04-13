7770f057dc83017e09f2fc5292d87f1ebcf53258
pepe78 <taraba.pepe@gmail.com>
2017-04-12 22:31:56 -0700

    adding 2d ball (circle)

--- Python/run_jg_henon.py
+++ Python/run_jg_henon.py
@@ -1,18 +1,22 @@
+from systems.BaseSystem import BaseSystem
 from systems.System2dHenon import System2dHenon
 from systems.System1dSimple import System1dSimple
+from systems.System2dBall import System2dBall
 from iMath.PointsGenerator import generate_points
 from iMath.JungeKevrekidis import JungeKevrekidis
 from iMath.Subdivision import Subdivision
 from iPlot.iPlot import iPlot
 
+#system = BaseSystem(0.5)
 system = System2dHenon()
 #system = System1dSimple(0.8)
+#system = System2dBall()
 
-attractor = Subdivision.do_subdivision(system, system.borders, 10)
+attractor = Subdivision.do_subdivision(system, system.borders, 8)
 
-num_points = 100
+num_points = 1000
 points = generate_points(num_points, system.borders)
-points = JungeKevrekidis(system, points, 50)
+points = JungeKevrekidis(system, points, 200)
 
 x_attractor = attractor.get_points()
 x = [[] for i in range(system.dimension)]
